b6f7148262886289d5800304db69af4e84410b6a
SE13 <se13codeur@gmail.com>
2021-11-27 13:52:20 +0100

    Initial Commit

---
+++ MyMovieDB/MyMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieDBApp.swift
+//  MyMovieDB
+//
+//  Created by SSS on 27/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
