6235482de3672b90919d4c1b219ef50db3cd922c
SE13 <se13codeur@gmail.com>
2021-11-28 16:54:28 +0100

    modified style of MovieCell

--- MyMovieDB/MovieCell.swift
+++ MyMovieDB/MovieCell.swift
@@ -6,23 +6,42 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct MovieCell: View {
     var body: some View {
         
         HStack {
-//            @todo: Image {}
-            VStack {
+            KFImage(URL(string:
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mpgDeLhl8HbhI03XLB7iKO6M6JE.jpg"))
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 150)
+                .clipped()
+                .cornerRadius(8.1)
+            VStack(alignment: .leading) {
                 HStack {
-                    VStack {
+                    VStack(alignment: .leading, spacing: 8.1) {
                         Text("Titre")
+                            .font(.title2)
+                            .fontWeight(.semibold)
                         Text("Saison + Types")
+                            .foregroundColor(Color.white.opacity(0.55))
+                            .font(.caption)
+                            .lineLimit(1)
                     }
+                    Spacer()
                     Text("Note")
                 }
                 Text("Descriptif")
+                    .font(.callout)
+                    .fontWeight(.light)
+                    .lineLimit(4)
             }
         }
+        .padding(8.1)
+        .background(Color(red: 22/255, green: 33/255, blue: 55/255))
+        .foregroundColor(.white)
             
     }
 }
