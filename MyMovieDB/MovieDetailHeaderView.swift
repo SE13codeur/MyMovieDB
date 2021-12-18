b86f083eb7b1a693742314d6fcd01f578086a31e
SE13 <se13codeur@gmail.com>
2021-12-18 09:28:53 +0100

    MovieDetailHeaderView

--- MyMovieDB/MovieDetailHeaderView.swift
+++ MyMovieDB/MovieDetailHeaderView.swift
@@ -18,21 +18,36 @@ struct MovieDetailHeaderView: View {
     
     var body: some View {
         VStack{
-            ZStack  {
+            ZStack(alignment: .bottom) {
                 KFImage(imageURL)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .frame(height: 300)
-                HStack {
-                    VStack {
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading, spacing: 8) {
                         Text(title)
-                        Text("\(seasonCount) episodes")
-                            .foregroundColor(Color.white.opacity(0.5))
-                            .font(.caption)
-                            .lineLimit(1)
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        Text("\(seasonCount) seasons" + " | " + genres.joined(separator: " • "))
+                            .foregroundColor(Color.white.opacity(0.6))
+                            .font(.footnote)
                     }
-                    //Note
+                    Spacer()
+                    CircleProgressBar(progress: rating)
+                        .frame(width: 45, height: 45)
+                        .padding(2.5)
                 }
+                .padding()
+                .padding(.top, 80)
+                .background(
+                    LinearGradient(
+                        gradient: Gradient(colors: [.clear,
+                                                    Color(red: 22/255, green: 33/255, blue: 55/255)]),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
             }
             // Description
             
@@ -45,8 +60,9 @@ struct MovieDetailHeaderView_Previews: PreviewProvider {
         MovieDetailHeaderView(imageURL: URL(string: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/eyTu5c8LniVciRZIOSHTvvkkgJa.jpg"),
                               title: "Bones",
                               seasonCount: 3,
-                              genres: ["Crime, Drama"],
-                              rating: 8.1)
+                              genres: ["Crime", "Drama"],
+                              rating: 8.1
+        )
             .previewLayout(.sizeThatFits)
 
     }
